e29b51ddd79f8f848a5f0ccb19071f11dfa80666
Kevin Mitchell <giudecca@gmail.com>
2026-01-17 03:26:24 -0500

    add deploy instructions

---
+++ pi-deploy/LAUNCH.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pkill -f "python3 -m http.server 8080"
+
+GAME_DIR="$HOME/Desktop/uddermayhem"
+cd "$GAME_DIR"
+python3 -m http.server 8080 &
+SERVER_PID=$!
+
+sleep 1
+chromium-browser --kiosk --noerrdialogs http://localhost:8080
+
+# When Chromium closes, kill the server
+kill $SERVER_PID
